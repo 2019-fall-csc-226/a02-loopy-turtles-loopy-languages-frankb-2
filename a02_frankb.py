80b4943004f0d5dde556a78c899ac3c9c6b7d157
frankb <frankb@berea.edu>
2019-09-10 23:37:19 -0400

    Spirals and defined functions. Added Repo, Google Doc, and Name to Readme Added Comments

--- a02_frankb.py
+++ a02_frankb.py
@@ -9,23 +9,34 @@
 import turtle
 
 
+######################################################################
+# define functions for drawing a polygon and repeating the polygon
+######################################################################
+
 def draw_shape(t, sz, n_side, m):
+    """
+    Draws a polygon and resets position of turtle
+    """
     t.rt(21)
     t.fd(m)
     for side in range(n_side):
-
         t.fd(sz)
         t.rt(360/n_side)
 
 
 def repeat_shape(polygon):
+    """
+    Takes the polygon and repeats it in a spiraling pattern
+    :param polygon:
+    :return:
+    """
     size = 10
     move = 10
     increase = 2
     bryar.setheading(0)
     megan.setheading(120)
     gwen.setheading(240)
-    for repetitions in range(40):
+    for repetitions in range(200//polygon):
         draw_shape(bryar, size, polygon, move)
         draw_shape(megan, size, polygon, move)
         draw_shape(gwen, size, polygon, move)
@@ -34,14 +45,15 @@ def repeat_shape(polygon):
         increase = increase + .9
 
 
-
-
-
-
-
-
+######################################################################
+# Ask User how many sides for polygon and save for the functions
+######################################################################
 num_sides = int(input("How many sides for repeating shapes: "))
 
+
+######################################################################
+# Create Screen, Turtles, and Settings for Each
+######################################################################
 wn = turtle.Screen()
 wn.setup(width=1366, height=768, startx=0, starty=0)
 wn.bgcolor('black')
@@ -62,6 +74,9 @@ bryar.pensize(10)
 gwen.pensize(10)
 megan.pensize(10)
 
+######################################################################
+# Fire that baby off!!!
+######################################################################
 repeat_shape(num_sides)
 
 wn.exitonclick()

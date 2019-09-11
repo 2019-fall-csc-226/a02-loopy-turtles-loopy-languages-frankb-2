ccde119ea21b235e9884ad1324fe25eb3d486344
frankb <frankb@berea.edu>
2019-09-10 22:24:25 -0400

    I Don't Know what I'm doing

--- a02_frankb.py
+++ a02_frankb.py
@@ -8,12 +8,28 @@
 # Importing turtle library, creating window, and defining two turtles
 import turtle
 
-wn = turtle.Screen()
-wn.bgcolor('black')
-bryar = turtle.Turtle()
-megan = turtle.Turtle()
-gwendolin = turtle.Turtle()
 
-bryar.color('cyan')
-gwendolin.color("Deep Sky Blue")
-megan.color("Firebrick")
+def draw_shape(name, head, polygon, size):
+    name.setheading(head)
+    for side in range(polygon):
+        name.fd(size)
+        name.rt(360/polygon)
+
+
+
+
+num_sides = int(input("How many sides for repeating shapes: "))
+
+bryar = turtle.Turtle()
+#megan = turtle.Turtle()
+#gwen = turtle.Turtle()
+bryar.color('Cyan')
+#gwen.color("Deep Sky Blue")
+#megan.color("Firebrick")
+wn = turtle.Screen()
+wn.setup(width=1366, height=768, startx=0, starty=0)
+wn.bgcolor('black')
+
+
+
+wn.exitonclick()
